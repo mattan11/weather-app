fc8196411e2c5827e8c48724fdaed99f082350e5
Marek Witkowski <marek.witkowski@onet.com.pl>
2020-08-05 10:20:12 +0200

    Updates

---
+++ precache-manifest.5009db0351c3f2c63338d34ddad84460.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "838c2d2b9eaabd50b8aca0c2f0b8d38e",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "31ad5f602d63d86f727f",
+    "url": "/weather-app/static/css/main.76dc21a2.chunk.css"
+  },
+  {
+    "revision": "db58191f6cac8616c7a1",
+    "url": "/weather-app/static/js/2.ef1d8a32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.ef1d8a32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31ad5f602d63d86f727f",
+    "url": "/weather-app/static/js/main.00b5dcb4.chunk.js"
+  },
+  {
+    "revision": "cba27d1f1c52bf941055",
+    "url": "/weather-app/static/js/runtime-main.d836fc30.js"
+  },
+  {
+    "revision": "2a86244f7e7a3a4a5e3800746203b6e0",
+    "url": "/weather-app/static/media/background.2a86244f.webp"
+  }
+]);
